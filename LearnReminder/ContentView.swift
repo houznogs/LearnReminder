93a6e2eb9f1832cdfcb6d790585d28d98236ef6c
Song <s259zhou@uwaterloo.ca>
2025-12-23 00:24:40 -0500

    Initial Commit

---
+++ LearnReminder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LearnReminder
+//
+//  Created by Song Zhou on 2025-12-23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LearnReminder/LearnReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnReminderApp.swift
+//  LearnReminder
+//
+//  Created by Song Zhou on 2025-12-23.
+//
+
+import SwiftUI
+
+@main
+struct LearnReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
